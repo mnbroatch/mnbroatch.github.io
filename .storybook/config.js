f9b831a2a0ed535d847b07d87d8154cd9b423e9b
Matthew Broatch <mnbroatch@gmail.com>
2019-11-13 20:26:20 -0800

    storybook config and DialogTree begun

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,4 @@
-import { configure } from '@storybook/react';
+import { addParameters, configure } from '@storybook/react';
 
+addParameters({ options: { isFullscreen: true, isToolshown: false } })
 configure(require.context('../components', true, /\.stories\.js$/), module);
---
+++ components/controls/DialogTree/index.js
@@ -0,0 +1,11 @@
+import React, { useState, useCallback, cloneElement } from 'react'
+
+import baseStyles from './baseStyles.js'
+
+export default function DialogTree ({
+}) {
+  return (
+    <div style={baseStyles.root}>
+    </div>
+  )
+}
