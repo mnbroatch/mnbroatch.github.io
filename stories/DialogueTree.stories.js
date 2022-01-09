7444f6293ff90ba3f60f1c58e37dbfc1a017a3c1
Matthew Broatch <mnbroatch@gmail.com>
2022-01-09 01:05:18 -0800

    copy story from react-dialogue-tree

---
+++ stories/DialogueTree.stories.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import DialogueTree from 'react-dialogue-tree'
+import './styles.css'
+
+const dialogue = `title: Start
+---
+I am a line
+I am a second line
+I am a third line
+I am a fourth line
+I am a fifth line
+I am a sixth line
+I am the last line!
+===`
+
+export default {
+  title: 'DialogueTree',
+  component: DialogueTree,
+  args: { dialogue },
+  argTypes: {
+    dialogue: {
+      control: 'text'
+    }
+  }
+}
+
+export const Template = (props) => {
+  return (
+    <div className="story">
+      <div className="dialogue-tree-container">
+        <DialogueTree
+          {...props}
+        />
+      </div>
+    </div>
+  )
+}
+
+export const Basic = Template.bind({})
